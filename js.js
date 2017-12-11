5e72cb880ee7c73313b9807ad308895e325878f4
Noah Heinrich <noahheinrich@gmail.com>
2017-12-11 09:04:58 -0600

    add more javascript, css

--- js.js
+++ js.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+  if ($(window).width() < 992) {
+      $("button").hide()
+      $("#nav-menu").removeClass("dropdown-menu")
+    } else {
+      $("button").show()
+      $("#nav-menu").addClass("dropdown-menu")
+    }
   $(window).resize(function(){
     if ($(window).width() < 992) {
       $("button").hide()
